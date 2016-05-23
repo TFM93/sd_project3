15a0021025ae00051d6c7fc4b6bb332a4591c9bf
ivopintodasilva <ivopintodasilva@gmail.com>
2016-05-23 16:59:28 +0100

    bench interface created;

---
+++ src/pt/ua/sd/RopeGame/interfaces/BenchInterface.java
@@ -0,0 +1,30 @@
+package pt.ua.sd.RopeGame.interfaces;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import pt.ua.sd.RopeGame.info.Bundle;
+import pt.ua.sd.RopeGame.info.VectorTimestamp;
+
+/**
+ * Created by ivosilva on 23/05/16.
+ */
+public interface BenchInterface extends Remote{
+
+    Bundle callContestants(int team_id,int[] selected_contestants, int n_players, VectorTimestamp vectorTimestamp) throws RemoteException;
+
+    Bundle informReferee(VectorTimestamp vectorTimestamp) throws RemoteException;
+
+    Bundle followCoachAdvice(int contestant_id,int strength, int team_id, int n_players, int n_players_pushing, VectorTimestamp vectorTimestamp) throws RemoteException;
+
+    Bundle getReady(int n_players_pushing, VectorTimestamp vectorTimestamp) throws RemoteException;
+
+    Bundle callTrial(VectorTimestamp vectorTimestamp) throws RemoteException;
+
+    Bundle startTrial(VectorTimestamp vectorTimestamp) throws RemoteException;
+
+    Bundle declareMatchWinner(int games1, int games2, VectorTimestamp vectorTimestamp) throws RemoteException;
+
+    void terminate() throws RemoteException;
+
+    boolean isClosed() throws RemoteException;
+
+}
