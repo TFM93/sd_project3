645a1b97812f5aa8c4eb40bd72a17222e509a677
ivopintodasilva <ivopintodasilva@gmail.com>
2016-05-23 17:25:12 +0100

    interfaces created;

---
+++ src/pt/ua/sd/RopeGame/interfaces/PlaygroundInterface.java
@@ -0,0 +1,28 @@
+package pt.ua.sd.RopeGame.interfaces;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import pt.ua.sd.RopeGame.info.Bundle;
+import pt.ua.sd.RopeGame.info.VectorTimestamp;
+import pt.ua.sd.RopeGame.structures.TrialStat;
+
+/**
+ * Created by ivosilva on 23/05/16.
+ */
+public interface PlaygroundInterface extends Remote {
+
+    Bundle reviewNotes(int[] selected_contestants, int n_players, int n_players_pushing, VectorTimestamp vectorTimestamp)throws RemoteException;
+
+    Bundle pullTheRope(int team_id, int strenght, int contestant_id, int n_players_pushing, int n_players, VectorTimestamp vectorTimestamp)throws RemoteException;
+
+    Bundle iAmDone(int n_players_pushing, VectorTimestamp vectorTimestamp)throws RemoteException;
+
+    Bundle seatDown(int n_players_pushing, VectorTimestamp vectorTimestamp)throws RemoteException;
+
+    Bundle assertTrialDecision(int n_players_pushing, int knockDif, VectorTimestamp vectorTimestamp)throws RemoteException;
+
+    void terminate() throws RemoteException;
+
+    boolean isClosed() throws RemoteException;
+
+
+}
---
+++ src/pt/ua/sd/RopeGame/interfaces/RefereeSiteInterface.java
@@ -0,0 +1,24 @@
+package pt.ua.sd.RopeGame.interfaces;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import pt.ua.sd.RopeGame.info.Bundle;
+import pt.ua.sd.RopeGame.info.VectorTimestamp;
+import pt.ua.sd.RopeGame.structures.GameStat;
+
+/**
+ * Created by ivosilva on 23/05/16.
+ */
+public interface RefereeSiteInterface extends Remote {
+
+    Bundle announceNewGame(VectorTimestamp vectorTimestamp)throws RemoteException;
+
+    Bundle declareGameWinner(int score_T1, int score_T2, int knock_out, int n_games, VectorTimestamp vectorTimestamp)throws RemoteException;
+
+    Bundle getN_games_played(VectorTimestamp vectorTimestamp)throws RemoteException;
+
+    void terminate() throws RemoteException;
+
+    boolean isClosed() throws RemoteException;
+
+
+}
